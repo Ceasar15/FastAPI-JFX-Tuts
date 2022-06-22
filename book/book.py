c445b4a66b91b955ec7d5ae4523534c31c7c4847
Ceasar <ceasarkwadwo@gmail.com>
2022-06-22 13:27:50 +0000

    implementing redis cache

--- book/book.py
+++ book/book.py
@@ -2,7 +2,35 @@ from sqlalchemy.orm import Session
 from core.schemas import schemas
 from core.models import models
 from fastapi import HTTPException
+import redis
+import sys
+from datetime import timedelta
 
+# connect to redis
+def connect_redis() -> redis.client.Redis:
+    try:
+        client = redis.Redis()
+        ping = client.ping()
+        if ping is None:
+            sys.exit("Connection Error")
+        else:
+            return client
+    except redis.exceptions.ConnectionError:
+        sys.exit("Connection Error")
+        
+print("Connecting to redis...", connect_redis())
+
+
+client = connect_redis()
+
+def get_book_from_cache(key: str):
+    cached_book = client.get(key)
+    return cached_book
+
+
+def set_book_in_cache(key: str, book: models.Book):
+    cached_book = client.set(key=key, value=book, timedelta=timedelta(hours=4))
+    return cached_book
 
 def create_book(request: schemas.BookSchema, db: Session):
     book = models.Book(
@@ -23,7 +51,11 @@ def get_all_books(db: Session):
     return books
 
 def get_specific_book(book_id: int, db: Session):
-    book = db.query(models.Book).filter(models.Book.id == book_id).all()
+    book = get_book_from_cache(book_id)
+
+    if book is None:
+        book = db.query(models.Book).filter(models.Book.id == book_id).all()
+
     return book
 
 def edit_book(book_id: int, request: schemas.BookSchema, db: Session):
