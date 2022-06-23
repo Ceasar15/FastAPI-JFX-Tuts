a7b3bcf082f08a3ff7fc5f4a778f5b887d4767df
Ceasar <ceasarkwadwo@gmail.com>
2022-06-23 14:19:23 +0000

    redis module

--- book/book.py
+++ book/book.py
@@ -1,76 +1,14 @@
-import asyncio
-from calendar import c
-from sqlalchemy.orm import Session
+import json
+
 from core.schemas import schemas
 from core.models import models
+from core.utils import redis
+
 from fastapi import HTTPException
-import redis
-import sys
-import json
-from typing import Any
+from sqlalchemy.orm import Session
 
 book_not_found = "Book not Found"
 
-# connect to redis
-
-
-def connect_redis() -> redis.client.Redis:
-    try:
-        client = redis.Redis(
-            host="localhost",
-            port=6379,
-            db=0,
-        )
-        ping = client.ping()
-        if ping is None:
-            sys.exit("Connection Error")
-        else:
-            return client
-    except redis.exceptions.ConnectionError:
-        sys.exit("Connection Error")
-
-
-print("Connecting to redis...", connect_redis())
-
-
-client = connect_redis()
-
-
-def get_book_from_cache(key=0):
-    if key:
-        cached_book = client.get(key)
-        print("result from cache one book")
-        return cached_book
-    else:
-        listed_result = []
-        for new_key in client.scan_iter():
-            listed_result.append(json.loads(
-                client.get(new_key).decode('utf-8')))
-        print("result from cache")
-        return listed_result
-
-
-async def set_book_in_cache(book: models.Book) -> bool:
-    for _ in range(0, len(book)):
-        data_dict = {
-            "time_created": str(book[_].time_created),
-            "price": book[_].price,
-            "title": book[_].title,
-            "id": book[_].id,
-            "description": book[_].description,
-            "author": book[_].author,
-        }
-
-        data = json.dumps(data_dict)
-        cached_book = client.setex(book[_].id, 4, data)
-    print("result from db")
-    return cached_book
-
-
-async def delete_book_from_cache(key: str):
-    res = client.delete(key)
-    print(444, res)
-    return res
 
 async def create_book(request: schemas.BookSchema, db: Session):
     books_from_db = models.Book(
@@ -86,23 +24,23 @@ async def create_book(request: schemas.BookSchema, db: Session):
 
 
 async def get_all_books(db: Session):
-    books_from_cache = get_book_from_cache()
+    books_from_cache = redis.get_book_from_cache()
     if not books_from_cache:
         books_from_db = db.query(models.Book).all()
-        await set_book_in_cache(books_from_db)
+        await redis.set_book_in_cache(books_from_db)
         return books_from_db
     else:
         return books_from_cache
 
 
 async def get_specific_book(book_id: int, db: Session):
-    book_from_cache = get_book_from_cache(book_id)
+    book_from_cache = redis.get_book_from_cache(book_id)
     if book_from_cache is None:
         book_from_db = db.query(models.Book).filter(
             models.Book.id == book_id).all()
         if not book_from_db:
             raise HTTPException(status_code=404, detail=book_not_found)
-        await set_book_in_cache(book_from_db)
+        await redis.set_book_in_cache(book_from_db)
         return book_from_db
 
     else:
@@ -138,5 +76,5 @@ async def delete_book(book_id: int, db: Session):
 
     db.delete(get_book)
     db.commit()
-    await delete_book_from_cache(book_id)
+    await redis.delete_book_from_cache(book_id)
     return get_book
